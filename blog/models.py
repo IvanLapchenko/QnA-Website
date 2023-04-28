230f9e10909701f3fb7f22ef3211d2370afb2204
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-04-28 21:50:33 +0300

    change post name to question and add answer model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     text = models.TextField()
@@ -18,3 +18,16 @@ class Post(models.Model):
         return self.title
 
 
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
