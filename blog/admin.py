dcce01be3f1c12c7c0e2b6969fdeb1f37b868b1e
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-04-28 21:53:49 +0300

    update admin to display question and answer

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Question, Answer
 
 
-admin.site.register(Post)
+admin.site.register(Question)
+admin.site.register(Answer)
 
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Post
+from .models import Question
 from .forms import AddPostForm
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Question.objects.all()
     print(posts)
     return render(request, 'post_list.html', {'posts': posts})
 
