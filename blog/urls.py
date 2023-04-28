88ca97c7844e32e84cad7c1ee0e9a312c9348abc
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-04-29 00:07:29 +0300

    change main template so it will display content properly; change request logic in render_m ain_page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import question_list, create_question, create_answer
+from .views import render_main_page, create_question, create_answer
 
 urlpatterns = [
-    path('', question_list, name='question_list'),
+    path('', render_main_page, name='question_list'),
     path('create_question', create_question, name='create_question'),
     path('create_answer', create_answer, name='create_answer')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,42 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import QuestionForm, AnswerForm
 from .models import Question, Answer
 
 
-def question_list(request):
-    questions = Question.objects.all().prefetch_related('answers')
-    context = {'questions': questions}
-    return render(request, 'main.html', context)
+def render_main_page(request):
+    questions = Question.objects.all().prefetch_related('answer_set')
+    question_form = QuestionForm()
+    answer_form = AnswerForm()
+    return render(request, 'main.html', {'questions': questions,
+                                         'question_form': question_form,
+                                         'answer_form': answer_form})
 
 
 def create_question(request):
-    form = QuestionForm()
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.author = request.user
             question.publish()
-            return redirect('question_detail', pk=question.pk)
-    return render(request, 'main.html', {'form': form})
+            messages.success(request, 'Your question has been posted successfully!')
+        else:
+            messages.error(request, 'There was an error posting your question. Please try again.')
+    return redirect('/')
 
 
-def create_answer(request, question_pk):
-    question = Question.objects.get(pk=question_pk)
+def create_answer(request):
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            question_pk = request.POST.get('question_pk')
+            question = Question.objects.get(pk=question_pk)
             answer.question = question
             answer.author = request.user
             answer.publish()
-            return redirect('question_detail', pk=question.pk)
-    else:
-        form = AnswerForm()
-    return render(request, 'main.html', {'form': form, 'question': question})
-
+            messages.success(request, 'Your answer has been posted successfully!')
+        else:
+            messages.error(request, 'There was an error posting your answer. Please try again.')
+    return redirect('/')
