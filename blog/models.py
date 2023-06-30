8ce0e9b99a998c26a249f09585a5ac704ea9b9cf
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-06-30 17:05:40 +0300

    refactor models. add voting logic

--- blog/models.py
+++ blog/models.py
@@ -1,41 +1,62 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
 
 
-class Question(models.Model):
+class BaseModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    title = models.CharField(max_length=128)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    upvotes = ArrayField(models.IntegerField(default=0), default=list)
+    downvotes = ArrayField(models.IntegerField(default=0), default=list)
+
+    def add_rating(self, user):
+        if user not in self.upvotes:
+            self.upvotes.append(user)
+            if user in self.downvotes:
+                self.downvotes.remove(user)
+        self.save()
+
+    def reduce_rating(self, user):
+        if user not in self.downvotes:
+            self.downvotes.append(user)
+            if user in self.upvotes:
+                self.upvotes.remove(user)
+        self.save()
+
+    def get_rating(self):
+        return len(self.upvotes) - len(self.downvotes)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    class Meta:
+        abstract = True
+
+
+class Question(BaseModel):
+    title = models.CharField(max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
 
 
-class Answer(models.Model):
+class Answer(BaseModel):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.text
