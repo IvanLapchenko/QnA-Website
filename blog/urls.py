9de6aaa9589719ee87fd6601f41a4a80eb949758
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-06-30 17:27:27 +0300

    add url for vote, forgot to add it in prev commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import render_main_page, create_question, create_answer, search, render_categories_page, \
-    render_question_page, edit_model, render_ask_question_page, delete_model, render_user_page
+    render_question_page, edit_model, render_ask_question_page, delete_model, render_user_page, vote
 
 urlpatterns = [
     path('', render_main_page, name='main_page'),
@@ -13,4 +13,5 @@ urlpatterns = [
     path('user_page', render_user_page, name='user_page'),
     path('<model>/edit/<int:record_id>/', edit_model, name='edit_record'),
     path('<model>/delete/<int:question_id>/', delete_model, name='delete_model'),
+    path('<model>/<action>/<int:record_id>/', vote, name='vote'),
 ]
